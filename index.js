88e1038aec8b4fad7bb7670c23d81ca3a5e4d801
TranThuy <147242503+ThuyThanhTranNg@users.noreply.github.com>
2025-03-13 07:34:20 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,19 @@
-// let car = {
-//     name: 'kien',
-//     speed: '10000000000',
-// }
+//định nghĩa express (function)
+const express = require('express');
 
-// console.log(car);
+//tạo ra 1 app
+const app = express();
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
+app.get('/', (req, res) =>{
+    //res trả về yêu cầu
+    //req lấy yêu cầu từ client
+    res.render('index')
+})
 
-// let content = "hello file node.js";
-// const fs = require("fs");
-
-// fs.writeFile("demo.txt","như hoa như nguyệt, như cứt", "utf8", (err)=>{
-//     console.log(err);
-    
-// })
-// fs.readFile("demo.txt", "utf8", (err, data) => {
-//     console.log(data); // In nội dung file
-// });
-
-// const http = require('node:http');
-
-// // Create a local server to receive data from
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   res.end(JSON.stringify({
-//     data: 'Hello World!',
-//   }));
-// });
-
-// server.listen(8000,()=>{
-//     console.log('http://localhost:8000');
-// });
-const os = require("os");
-const fs = require("fs");
-const path = require("path");
-const http = require("http");
-
-// Lấy thông tin cấu hình máy tính
-const systemInfo = {
-    OSType: os.type(),
-    Platform: os.platform(),
-    RAM: os.totalmem(),
-    USEDRAM: os.totalmem() - os.freemem(),
-    CPU: os.cpus(),
-};
-
-// Lưu thông tin vào file
-const folderPath = "C:/LTMNM";
-const filePath = path.join(folderPath, "systemInfo.json");
-
-fs.mkdir(folderPath, { recursive: true }, (err) => {
-    if (err) {
-        console.error("Lỗi tạo thư mục:", err);
-        return;
-    }
-    fs.writeFile(filePath, JSON.stringify(systemInfo, null, 2), (err) => {
-        if (err) {
-            console.error("Lỗi ghi tệp:", err);
-            return;
-        }
-        console.log("Ghi tệp thành công!");
-        console.log("Completed task!");
-    });
-});
-
-// Tạo server hiển thị thông tin
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(systemInfo, null, 2));
-});
-
-// Server chạy trên cổng 3000
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server đang chạy tại http://localhost:${PORT}`);
-});
-
+//tạo ra cổng
+app.listen(3000, () =>{
+    console.log('server connected');
+})
